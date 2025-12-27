b33d7c6fb55c60218add49ef97b9a111a8a82c1b
Dmitry Osin <d@osin.pro>
2025-12-27 04:13:21 +0300

    [~] publish to store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "pro.osin.tools.medicare"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
